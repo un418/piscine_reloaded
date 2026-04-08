0be158f205fd8663aa31b9af00d3f0da912d6f64
Adam Fernandes <adaferna@student.42lisboa.com>
2026-04-08 22:00:49 +0100

    add ex06

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/08 20:43:49 by adaferna          #+#    #+#             */
+/*   Updated: 2026/04/08 21:50:38 by adaferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* 
+Create a function that displays the alphabet in lowercase, on a single line, by
+ascending order, starting from the letter ’a’.
+*/
+
+void	ft_print_alphabet(void)
+{
+	char	c;
+
+	c = 'a';
+	while (c < 'z')
+	{
+		ft_putchar(c);
+		c++;
+	}
+}
+
+/*
+int	main(void)
+{
+	ft_print_alphabet();
+}
+*/
